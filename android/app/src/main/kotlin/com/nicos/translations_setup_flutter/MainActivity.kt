3fa77437cbfc9fe9c081644e63571be3b9b531b1
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-01-27 22:32:21 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/nicos/translations_setup_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nicos.translations_setup_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:translations_setup_flutter/my%20_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
